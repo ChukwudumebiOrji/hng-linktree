ce7bcbc57a44563766122a1d7243a770c8e43063
ChukwudumebiOrji <111486356+ChukwudumebiOrji@users.noreply.github.com>
2022-11-06 13:24:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,17 @@ import "./index.css"
 import App from "./App"
 import { ContactPage } from "./pages/ContactPage"
 import {
-  createBrowserRouter,
-  createRoutesFromChildren,
-  Route,
-  RouterProvider,
+  BrowserRouter, Routes, Route 
 } from "react-router-dom"
-
-const router = createBrowserRouter(
-  createRoutesFromChildren(
-    <Route path="/">
-      <Route index element={<App />}></Route>
-      <Route path={"/contact"} element={<ContactPage />}></Route>
-    </Route>
-  )
-)
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 )
